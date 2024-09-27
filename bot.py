3855df1bb7716b0d551cf121fa65b0c478a22380
elition83 <110487186+elition83@users.noreply.github.com>
2024-09-27 23:27:55 +0300

    Update bot.py
    Перевод коментов на русский

--- bot.py
+++ bot.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python
 # pylint: disable=unused-argument
-# This program is dedicated to the public domain under the CC0 license.
+# This work is dedicated to the public domain under the Creative Commons CC0 License.
+.
 
 """
-Simple Bot to reply to Telegram messages.
+Простой бот для ответа на сообщения Telegram.
 
-First, a few handler functions are defined. Then, those functions are passed to
-the Application and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
+Сначала определяются несколько функций-обработчиков. Затем эти функции передаются
+приложению и регистрируются в соответствующих местах.
+Затем бот запускается и работает до тех пор, пока мы не нажмем Ctrl-C в командной строке.
 
-Usage:
-Basic Echobot example, repeats messages.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
 """
 
 import logging
@@ -26,14 +23,13 @@ from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandl
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
-# set higher logging level for httpx to avoid all GET and POST requests being logged
+# установите более высокий уровень регистрации для httpx, чтобы не регистрировать все GET- и POST-запросы
 logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
 
-# Define a few command handlers. These usually take the two arguments update and
-# context.
+# Определите несколько обработчиков команд. Обычно они принимают два аргумента update и контекст
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /start is issued."""
     user = update.effective_user
@@ -57,17 +53,17 @@ def main() -> None:
     """Start the bot."""
     load_dotenv()
 
-    # Create the Application and pass it your bot's token.
+    # Создайте приложение и передайте ему токен вашего бота.
     application = Application.builder().token(os.environ["TOKEN"]).build()
 
-    # on different commands - answer in Telegram
+    # на разные команды - ответ в Telegram
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
 
-    # on non command i.e message - echo the message on Telegram
+    # при отсутствии команды, т.е. сообщения - отправьте эхо сообщения в Telegram
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
-    # Run the bot until the user presses Ctrl-C
+    # Запускайте бота до тех пор, пока пользователь не нажмет Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
