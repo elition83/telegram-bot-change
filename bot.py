4eff0475f86d0dae3a99e044d78117884b0f21e7
elition <elition@mail.ru>
2024-12-09 22:36:38 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -96,12 +96,15 @@ def get_exchange_rate(base_currency: str) -> dict:
 # Обработчик команды /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     if update.message:
-        await update.message.reply_markup = ReplyKeyboardRemove()
-        await update.message.reply_text("Привет! Выберите действие:", reply_markup=get_main_keyboard())
+        await update.message.reply_text(
+            "Привет! Выберите действие:", reply_markup=get_main_keyboard()
+        )
     elif update.callback_query:
         query = update.callback_query
-        await query.message.reply_markup = ReplyKeyboardRemove()
-        await query.message.reply_text("Привет! Выберите действие:", reply_markup=get_main_keyboard())
+        await query.edit_message_text(
+            "Привет! Выберите действие:", reply_markup=get_main_keyboard()
+        )
+
 
 # Обработчик кнопки "Текущий курс"
 async def current_rate(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
